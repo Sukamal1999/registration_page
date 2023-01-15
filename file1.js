d9298aa68fcb92330366ac7652266e8b10445d8b
Sukamal <sukamalmondal07@gmail.com>
2023-01-15 18:23:23 +0530

    https://github.com/Sukamal1999/registration_page.git

---
+++ file1.js
@@ -0,0 +1,13 @@
+// function regform(){
+//     var name = document.getElementById("fname").value;
+//     var name1= document.getElementById("lname").value;
+//     var email= document.getElementById("email").value;
+//     var date  = document.getElementById("date").value;
+//     var time = document.getElementById("time").value;
+
+//     console.log("Your first Name is",name);
+//     console.log("Your first Name is",name);
+//     console.log("Your first Name is",name); 
+//     console.log("Your first Name is",name);  
+//         }
+
